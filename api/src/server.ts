771369798ebcf30034ef193e8ac6c1a2f714c4ee
rfmiotto <renato.fmiotto@gmail.com>
2026-01-18 19:47:39 -0300

    configure API

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,46 @@
-import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import ScalarApiReference from "@scalar/fastify-api-reference";
+import { fastify } from "fastify";
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+	origin: true,
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+	// credentials: true  // send authentication cookie headers from frontend to backend automatically if they share the same url
+});
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "Webhook Inspector API",
+			description: "API for capturing and inspecting webhook requests",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+	routePrefix: "/docs",
+});
 
 app
-  .listen({
-    port: 3333,
-    host: '0.0.0.0', // this allow us to host the app in Railway
-  })
-  .then(() => {
-    console.log('HTTP server is running!')
-  })
+	.listen({
+		port: 3333,
+		host: "0.0.0.0", // this allow us to host the app in Railway
+	})
+	.then(() => {
+		console.log("HTTP server is running on http://localhost:3333");
+		console.log("Docs available at http://localhost:3333/docs");
+	});
