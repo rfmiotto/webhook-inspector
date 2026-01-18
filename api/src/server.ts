788b9d074b0e9dce85767d89ae64c1da9776d93a
rfmiotto <renato.fmiotto@gmail.com>
2026-01-18 19:15:06 -0300

    configure biome for formatting

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,12 @@
-import { fastify } from "fastify"
+import { fastify } from 'fastify'
 
 const app = fastify()
 
-app.listen({
-  port: 3333,
-  host: "0.0.0.0" // this allow us to host the app in Railway
-}).then(() => {
-  console.log("HTTP server is running!")
-})
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0', // this allow us to host the app in Railway
+  })
+  .then(() => {
+    console.log('HTTP server is running!')
+  })
